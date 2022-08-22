2e3d76fd593e889a3aede18ed947978280a63718
Grant Mitchell <soundbreakr1@gmail.com>
2022-08-22 16:00:43 -0700

    learning to deal with json in java

--- server/src/core/App.java
+++ server/src/core/App.java
@@ -1,18 +1,52 @@
 package core;
 import org.joda.time.LocalTime;
-import org.json.simple.JSONObject;
-
+import com.cedarsoftware.util.io.JsonWriter;
 public class App {
 
 
   public static void main(String[] args){
     // call class to start up server
+
+
+/*
+methods on building json objects
+constructor variables
+> obj // jsonstring of obj
+> java obj
+
+createFromText
+main() makes empty {};
+
+*/
+
+    class User {
+      private  int age ;
+      private String gender;
+      private String city;
+      private Un nestedObj;
+
+      public User(int a, String b, String c, int d){
+        age = a;
+        gender = b;
+        city = c;
+        nestedObj = new Un(d);
+      }
+
+      class Un {
+        private int number;
+        public Un(int a){
+          number = a;
+        }
+      }
+    }
+    User user1 = new User(22, "male","phoenix",44);
+
+    String json = JsonWriter.objectToJson(user1);
+
+    System.out.println(json);
+
     LocalTime currentTime = new LocalTime();
-    JSONObject jsonObject = new JSONObject();
-    String text = "Text with special character /\"\'\b\f\t\r\n.";
-    System.out.println(text);
-    System.out.println("After escaping.");
-    text = jsonObject.escape(text);
+    String text = "Text with special character";
     System.out.println(text);
     System.out.println("The current local time is: " + currentTime);
     System.out.println("server main fn.");
---
+++ server/src/core/router/router.java
@@ -0,0 +1,10 @@
+package core.router;
+
+public class router {
+
+  public void Server(){
+    System.out.println("Fake Server starting on 8080");
+  }
+
+
+}
